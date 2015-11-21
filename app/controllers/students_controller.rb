728ba64a14c1858697e64986b4644f02179ed711
Jordan Hudgens <jordan@wow.com>
2015-11-21 14:48:51 -0600

    Implemented solution

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,24 @@
 class StudentsController < ApplicationController
+  before_action :set_student, only: :show
   
   def index
     @students = Student.all
   end
 
   def show
-    @student = Student.find(params[:id])
   end
 
+  def new
+  end
+
+  def create
+    session[:form_params] = params.inspect
+    redirect_to new_student_path
+  end
+
+  private
+
+    def set_student
+      @student = Student.find(params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :students, only: :index
-  get '/students/:id', to: 'students#show', as: 'student'
+  resources :students, only: [:index, :new, :create]
+  get '/student/:id', to: 'students#show', as: 'student'
 end
