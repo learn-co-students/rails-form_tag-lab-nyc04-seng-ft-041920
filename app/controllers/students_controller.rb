fb1de112a7e8a8ec832dedafcde3c6f5ce9ca2d7
Joe Burgess <joe@flatironschool.com>
2015-12-28 09:43:30 -0500

    removing before_action in students controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,16 +1,11 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
   
   def index
     @students = Student.all
   end
 
   def show
+    @student = Student.find(params[:id])
   end
 
-  private
-
-    def set_student
-      @student = Student.find(params[:id])
-    end
 end
