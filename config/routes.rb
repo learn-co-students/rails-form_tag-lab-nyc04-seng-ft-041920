6539dc5bda5e6ab9bfae6b49e5abffd63df62264
Howard <howarddevennish@gmail.com>
2017-11-28 11:05:24 -0500

    Change show route to follow RESTful convention
    Changed `get '/student/:id'` to `get '/students/:id'`

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   resources :students, only: :index
-  get '/student/:id', to: 'students#show', as: 'student'
+  get '/students/:id', to: 'students#show', as: 'student'
 end
