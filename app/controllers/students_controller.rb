2bce65d8614471a8039c88f6bd90dbe4a885dc7a
“dinadeljanin” <ddeljanin5@icloud.com>
2020-06-06 19:44:16 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,15 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
 
   def show
     @student = Student.find(params[:id])
+  end
+
+  def create
+    Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
   end
 
 end
